8568706ee6711174f7f8d4046f21e527027cbc53
Borysiuk1 <43035468+Borysiuk1@users.noreply.github.com>
2019-05-29 11:58:29 +0300

    Add files via upload

---
+++ lab1(3).cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+
+{
+ int n, m, mid, res=-1;
+ cin >> n >> m;
+ vector<int> a(n);
+ vector<int> b(m);
+ for (int i = 0; i < n; i++)
+  {
+    cin >> a[i];
+  }
+  for (int i = 0; i < m; i++)
+  {
+    cin >> b[i];
+  }
+  int resl;
+  for (int i = 0; i < m; i++)
+    {
+    	 res=-1;
+  int l = 0, r = n - 1;
+  while(l <= r) //ліва частина не може бути більшою за праву незалежно від сторони
+        {
+            mid = (l + r) / 2;
+            if (a[mid] == b[i])
+     		 {
+ 			   res=mid;
+ 			   r=mid-1; //пошук в лівій частині
+ 		     }
+           	   else if(a[mid]>b[i])
+             {
+                r = mid-1;
+             }
+             else
+             {
+                l=mid+1;
+			 }
+         }
+	l=0;
+	r=n-1;
+
+	while(l <= r)
+        {
+            mid = (l + r) / 2;
+            if (a[mid] == b[i])
+     		 {
+ 			   resl=mid;
+ 			   l=mid+1; //пошук в правій частині
+ 		     }
+           	   else if(a[mid]>b[i])
+             {
+               r=mid-1;
+             }
+             else
+             {
+            	l=mid+1;
+			 }
+         }
+         if(res!=-1)
+         {
+         	res++;
+         	resl++;
+         	cout<<res<<" "<<resl<<endl;
+		 }
+		 else
+		 {
+		 	cout<<"0"<<endl;
+		 }
+    }
+}
